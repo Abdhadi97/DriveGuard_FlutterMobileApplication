891952da9f5d785321e0ce89221c5184ba14849a
Abdhadi97 <2021899622@student.uitm.edu.my>
2024-05-16 04:47:11 +0800

    add overlay request page in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
-  int currentPageIndex = 0;
+  bool isOverlayVisible = false;
+  double overlayHeight = 0.2; // Initial height of the overlay
 
   //sign user out method
   void signUserOut() {
@@ -29,98 +30,180 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text("DriveGuard"),
-        actions: [
-          IconButton(
-            onPressed: signUserOut,
-            icon: const Icon(Icons.logout),
-            color: Colors.white,
-          )
-        ],
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: Stack(
+          children: [
+            ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                const SizedBox(
+                  height: 100, // Adjust the height as needed
+                  child: DrawerHeader(
+                    child: Text(
+                      'Drawer Head',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.account_circle),
+                  title: const Text('My profile'),
+                  onTap: () {
+                    // Navigate to profile page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ProfilePage(),
+                      ),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.pin_drop),
+                  title: const Text('Location'),
+                  onTap: () {
+                    // Navigate to location page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LocationTest(),
+                      ),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.history),
+                  title: const Text('History'),
+                  onTap: () {
+                    // Navigate to history page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Container(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: FloatingActionButton(
+                onPressed: () {
+                  signUserOut();
+                },
+                child: const Icon(Icons.logout),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: _getBody(currentPageIndex),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.amber,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
+      body: Stack(
+        children: [
+          // Map Content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'LOGGED IN AS: ${user.email!}',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
           ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.pin_drop),
-            icon: Icon(Icons.pin_drop_outlined),
-            label: 'Location',
+
+          // Draggable Overlay: Request page
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                setState(() {
+                  overlayHeight -= details.primaryDelta! /
+                      MediaQuery.of(context).size.height;
+                  overlayHeight = overlayHeight.clamp(
+                      0.2, 0.8); // Clamp the height between 20% and 80%
+                });
+              },
+              onVerticalDragEnd: (details) {
+                setState(() {
+                  // Snap the overlay to 80% if it's greater than 0.6
+                  if (overlayHeight <= 0.2) {
+                    overlayHeight = 0.2;
+                  } else if (overlayHeight > 0.2 && overlayHeight <= 0.5) {
+                    overlayHeight = 0.5;
+                  } else if (overlayHeight > 0.5) {
+                    overlayHeight = 0.8;
+                  }
+                });
+              },
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(15),
+                ),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * overlayHeight,
+                  color: Colors.white,
+                  child: SingleChildScrollView(
+                    physics: overlayHeight >= 0.8
+                        ? const NeverScrollableScrollPhysics()
+                        : null,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 20),
+                          const Text(
+                            'Request Assistance',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            'Need an Assistance? Get it with just one click.',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              letterSpacing: -0.5,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'See Available Workshops',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.list),
-            icon: Icon(Icons.list_outlined),
-            label: 'History',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.person_2),
-            icon: Icon(Icons.person_2_outlined),
-            label: 'Profile',
-          )
         ],
       ),
     );
-  }
-
-  Widget _getBody(int index) {
-    switch (index) {
-      case 0:
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'LOGGED IN AS: ${user.email!}',
-                style: const TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-              /*ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LocationTest(),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  minimumSize: const Size(100, 40),
-                ),
-                child: const Text('See my location'),
-              ),*/
-            ],
-          ),
-        );
-      case 1:
-        // Navigate to Location page
-        return const LocationTest(); // Assuming LocationTest is the page you want to navigate to
-      case 2:
-        // Navigate to History page
-        return Container(); // Placeholder for History page
-      case 3:
-        // Navigate to Profile page
-        return const ProfilePage(); // Placeholder for Profile page
-      default:
-        return Container(); // Placeholder for other pages
-    }
   }
 }
