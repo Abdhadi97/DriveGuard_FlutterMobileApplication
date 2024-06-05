539eba50fb418ad3a83ca77cfece770c9dea00af
Abdhadi97 <2021899622@student.uitm.edu.my>
2024-06-05 15:39:17 +0800

    modify launcher icon

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drive_guard/components/map_widget.dart';
 import 'package:drive_guard/pages/login_page.dart';
-import 'package:drive_guard/pages/profile_page.dart';
+import 'package:drive_guard/pages/profile_screen/profile_page.dart';
 import 'package:drive_guard/pages/workshop_list.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
